12fd018e2455654e9d23c4f3cce941d92a703062
Maxim Zimin <Makmizima398@mail.ru>
2022-06-03 09:03:56 +0300

    fixes + functions

--- js/Timer_Module.js
+++ js/Timer_Module.js
@@ -2,6 +2,7 @@
 
 // Получение элементов из документа
 const body = document.querySelector("body");
+const head = document.querySelector("head");
 
 
 // Создание элементов
@@ -10,16 +11,37 @@ const mainTimer = document.createElement("div");
 const btnChange = document.createElement("button");
 
 
+function addStyle(){
+    const style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.href = "Timer_Styles.css";
+    style.type = "text/css";
+    head.appendChild(style);
+}
+
 
 function createTimer(){
     const timer = document.createElement("div");
+    timer.classList.add("timer");
     // timer.textContent = "00:00:00";
 
-    const seconds = document.createElement("div").classList.add("time-block");
-    const minutes = document.createElement("div").classList.add("time-block");
-    const hours = document.createElement("div").classList.add("time-block");
+    const seconds = document.createElement("div");
+    const minutes = document.createElement("div");
+    const hours = document.createElement("div");
+
+    seconds.textContent = "00";
+    minutes.textContent = "00";
+    hours.textContent = "00";
+
+    seconds.classList.add("time-block", "seconds");
+    minutes.classList.add("time-block", "minutes");
+    hours.classList.add("time-block", "hours");
+
+    timer.appendChild(hours);
+    timer.appendChild(minutes);
+    timer.appendChild(seconds);
     
-    
+    return timer;
 }
 
 function createBtnChange(){
@@ -35,8 +57,8 @@ function changeTimer(btn, timer){
 
 function createMainTimer(){
 
-    mainTimer.appendChild(btnChange);
-    mainTimer.appendChild(timer);
+    // mainTimer.appendChild(btnChange);
+    mainTimer.appendChild(createTimer());
 
 
     mainTimer.style = "height: 50px; background-color: red;";
@@ -45,5 +67,5 @@ function createMainTimer(){
 
 
 
-
-// createMainTimer();
+addStyle();
+createMainTimer();
