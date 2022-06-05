17916053b0a58459bd7f3034acc57949bc9f6e29
Maxim Zimin <Makmizima398@mail.ru>
2022-06-05 20:51:46 +0300

    done without good design

--- js/Timer_Module.js
+++ js/Timer_Module.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 // Получение элементов из документа
 const body = document.querySelector("body");
 const head = document.querySelector("head");
@@ -10,6 +12,7 @@ const mainTimer = document.createElement("div");
 // const timer = document.createElement("div");
 // const btnChange = document.createElement("button");
 
+let interval;
 
 function addStyle(){
     const style = document.createElement("link");
@@ -48,30 +51,90 @@ function createTimer(){
     return timer;
 }
 
+
 function createBtnChange(){
     const btnChange = document.createElement("button");
+    btnChange.textContent = "Изменить";
     btnChange.addEventListener("click", ()=>{
-        const timer = document.querySelector(".timer");
-        document.querySelector(".seconds").contentEditable = true;
+
+        let hours = btnChange.parentNode.firstChild.firstChild;
+        let minutes = btnChange.parentNode.firstChild.childNodes[1];
+        let seconds = btnChange.parentNode.firstChild.lastChild;
+
+        if (hours.contentEditable == "false"){
+            hours.contentEditable = true;
+            minutes.contentEditable = true;
+            seconds.contentEditable = true;
+            btnChange.textContent = "Сохраниь";
+        } else {
+            hours.contentEditable = false;
+            minutes.contentEditable = false;
+            seconds.contentEditable = false;
+            btnChange.textContent = "Изменить";
+        }
+
     });
 
-    btnChange.textContent = "Изменить";
+
     return btnChange;
 }
 
+function createBtnStart(){
+    const btnStart = document.createElement("button");
+    btnStart.textContent = "Старт";
+    btnStart.addEventListener("click", (e)=>{
 
-function changeTimer(btn, timer){
+        interval = setInterval((e)=>{
+            let hours = btnStart.parentNode.firstChild.firstChild;
+            let minutes = btnStart.parentNode.firstChild.childNodes[1];
+            let seconds = btnStart.parentNode.firstChild.lastChild;
+            let hV = parseInt(hours.textContent);
+            let mV = parseInt(minutes.textContent);
+            let sV = parseInt(seconds.textContent);
 
+            sV--;
+            if (sV < 0){
+                sV = 59;
+                mV--;
+                if (mV < 0){
+                    hV--;
+                    if (hV < 0){
+                        hV = 0;
+                        mV = 0;
+                        sV = 0;
+                        clearInterval(interval);
+                    }
+                }
+            }
+
+            hours.textContent = hV;
+            minutes.textContent = mV;
+            seconds.textContent = sV;
+        }, 1000);
+
+    });
+
+    return btnStart;
 }
 
 
+function createBtnStop(){
+    const btnStop = document.createElement("button");
+    btnStop.textContent = "Стоп";
+    btnStop.addEventListener("click", (e)=>{
+        clearInterval(interval);
+    });
+
+    return btnStop;
+}
 
 function createMainTimer(){
 
     
     mainTimer.appendChild(createTimer());
     mainTimer.appendChild(createBtnChange());
-
+    mainTimer.appendChild(createBtnStart());
+    mainTimer.appendChild(createBtnStop());
     mainTimer.style = "height: 50px; background-color: red;";
     body.appendChild(mainTimer);
 }
